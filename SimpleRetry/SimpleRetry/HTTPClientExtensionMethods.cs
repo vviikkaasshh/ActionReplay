15aa34b712979fef249b55c1826622a3f404a7b7
vikash <46368219+vviikkaasshh@users.noreply.github.com>
2020-05-27 17:23:05 +0530

    First initial commit for Use of Socket HTTP Handler. Some HTTP Extension methods

---
+++ SimpleRetry/SimpleRetry/HTTPClientExtensionMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleRetry
+{
+  public static class HTTPClientExtensionMethods
+  {
+    public static Task<HttpResponseMessage> GetAsync(this HttpClient client, string requestUri, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.GetAsync(requestUri, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> GetAsync(this HttpClient client, Uri requestUri, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.GetAsync(requestUri, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> PostAsync(this HttpClient client, string requestUri, HttpContent httpContent, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.PostAsync(requestUri, httpContent, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> PostAsync(this HttpClient client, Uri requestUri, HttpContent httpContent, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.PostAsync(requestUri, httpContent, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> PutAsync(this HttpClient client, string requestUri, HttpContent httpContent, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.PutAsync(requestUri, httpContent, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> PutAsync(this HttpClient client, Uri requestUri, HttpContent httpContent, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.PutAsync(requestUri, httpContent, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> DeleteAsync(this HttpClient client, string requestUri, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.DeleteAsync(requestUri, cts.Token);
+    }
+
+    public static Task<HttpResponseMessage> DeleteAsync(this HttpClient client, Uri requestUri, TimeSpan timeout)
+    {
+      var cts = new CancellationTokenSource(timeout);
+      return client.DeleteAsync(requestUri, cts.Token);
+    }
+  }
+}
---
+++ SimpleRetry/SimpleRetry/RetryClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleRetry
+{
+  public class RetryClient
+  {
+    private static HttpClient _client;
+
+    public static void GetClient()
+    {
+      _client = new HttpClient();
+    }
+
+    public static void GetInstance(TimeSpan connectionIdleTimeout, TimeSpan connectionLifetime)
+    {
+      SocketsHttpHandler socketsHttpHandler = new SocketsHttpHandler()
+      {
+        PooledConnectionIdleTimeout = connectionIdleTimeout,
+        PooledConnectionLifetime = connectionLifetime
+      };
+      _client = new HttpClient(socketsHttpHandler);
+    }
+  }
+}
